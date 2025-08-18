ce1704850f7daaa9371896a64a32b3d81177b91b
0xtbug <tubagus.nabil19@gmail.com>
2025-08-19 01:13:43 +0700

    feat(seo): add comprehensive metadata for improved search and social sharing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,57 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { GeistSans } from "geist/font/sans"
-import { GeistMono } from "geist/font/mono"
-import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import type React from "react";
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "Bookmark Manager",
   description: "A modern, responsive bookmark manager built with Next.js",
-}
+  icons: {
+    icon: "/cat.png",
+  },
+  openGraph: {
+    title: "Bookmark Manager",
+    description: "A modern, responsive bookmark manager built with Next.js",
+    url: "https://bm.tubagusnm.com",
+    siteName: "Bookmark Manager",
+    images: [
+      {
+        url: "/og-img.png",
+        width: 800,
+        height: 600,
+        alt: "og image",
+      },
+      {
+        url: "/og-img.png",
+        width: 1800,
+        height: 1600,
+        alt: "og image",
+      },
+    ],
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
@@ -28,11 +65,16 @@ html {
         `}</style>
       </head>
       <body>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
           {children}
           <SpeedInsights />
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
